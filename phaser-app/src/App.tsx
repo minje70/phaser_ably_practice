95a29a281a0219d9dc60859f65ba074151c8a3ad
minje70 <minje70@naver.com>
2022-01-04 22:51:37 +0900

    phaser-app이란 연습 폴더 삭제

--- phaser-app/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from 'react';
-import { config } from '../src/script';
-
-export default function App() {
-	useEffect(() => {
-		new Phaser.Game(config);
-	});
-	return <div></div>;
-}
